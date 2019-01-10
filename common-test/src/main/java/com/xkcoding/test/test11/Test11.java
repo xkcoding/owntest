1dca87852ce0ffe720844368968d5a0fee869c41
Yangkai.Shen <237497819@qq.com>
2019-01-10 17:56:52 +0800

    :sparkles: 测试Hutool的JsonArray对象转List

---
+++ common-test/src/main/java/com/xkcoding/test/test11/Test11.java
@@ -0,0 +1,38 @@
+package com.xkcoding.test.test11;
+
+import cn.hutool.json.JSONUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 测试Hutool的JsonArray对象转List
+ * </p>
+ *
+ * @package: com.xkcoding.test.test11
+ * @description: 测试Hutool的JsonArray对象转List
+ * @author: yangkai.shen
+ * @date: Created in 2019-01-10 17:37
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public class Test11 {
+    public static void main(String[] args) {
+        String jsonArr = "[{\"id\":111,\"name\":\"test1\"},{\"id\":112,\"name\":\"test2\"}]";
+        List<User> userList = JSONUtil.toList(JSONUtil.parseArray(jsonArr), User.class);
+
+        System.out.println(userList);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    class User {
+        private Integer id;
+        private String name;
+    }
+}
