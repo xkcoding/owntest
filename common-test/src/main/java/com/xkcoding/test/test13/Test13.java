057e8c473bf0ae2c8bee54d525a0286e6f6ff214
Yangkai.Shen <237497819@qq.com>
2019-01-24 16:01:38 +0800

    :sparkles: 测试 Disruptor

---
+++ common-test/src/main/java/com/xkcoding/test/test13/Test13.java
@@ -0,0 +1,20 @@
+package com.xkcoding.test.test13;
+
+/**
+ * <p>
+ * 测试 Disruptor
+ * </p>
+ *
+ * @package: com.xkcoding.test.test13
+ * @description: 测试 Disruptor
+ * @author: yangkai.shen
+ * @date: Created in 2019-01-24 16:00
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public class Test13 {
+    public static void main(String[] args) {
+        System.err.println("测试 Disruptor");
+    }
+}
