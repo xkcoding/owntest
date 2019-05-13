4cff77607926b086cbe4cbd43a994c928712fd3a
Yangkai.Shen <237497819@qq.com>
2019-05-13 22:16:55 +0800

    :sparkles: 测试 hutool 的 HttpUtil

---
+++ common-test/src/main/java/com/xkcoding/test/test26/Test26.java
@@ -0,0 +1,17 @@
+package com.xkcoding.test.test26;
+
+/**
+ * <p>
+ * 测试 hutool 的 HttpUtil
+ * </p>
+ *
+ * @package: com.xkcoding.test.test26
+ * @description: 测试 hutool 的 HttpUtil
+ * @author: yangkai.shen
+ * @date: Created in 2019-05-09 09:07
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public class Test26 {
+}
