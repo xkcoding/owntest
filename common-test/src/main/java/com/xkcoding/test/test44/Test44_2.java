b606c2a881a099fec93d972e634a5ccd7d3f17c5
Yangkai.Shen <237497819@qq.com>
2020-03-05 19:06:23 +0800

    :sparkles: 测试PDF中文乱码解决

--- common-test/src/main/java/com/xkcoding/test/test44/Test44_2.java
+++ common-test/src/main/java/com/xkcoding/test/test44/Test44_2.java
@@ -1,6 +1,11 @@
 package com.xkcoding.test.test44;
 
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.URLUtil;
 import org.docx4j.fonts.PhysicalFonts;
+
+import java.io.File;
+import java.net.URL;
 
 /**
  * <p>
@@ -14,15 +19,17 @@ public class Test44_2 {
     public static void main(String[] args) throws Exception {
         // 加载系统字体
         PhysicalFonts.discoverPhysicalFonts();
+        final URL resource = URLUtil.getURL(new File("/Users/yangkai.shen/Documents/code/back-end/owntest/common-test/src/main/resources/test44/SimSun.ttf"));
+        PhysicalFonts.addPhysicalFonts("SimSun", resource);
         exists("隶书", "LiSu");
         exists("宋体", "SimSun");
-        exists("微软雅黑", "Microsoft Yahei");
-        exists("黑体", "SimHei");
-        exists("楷体", "KaiTi");
-        exists("新宋体", "NSimSun");
+        exists("微软雅黑", "SimSun");
+        exists("黑体", "SimSun");
+        exists("楷体", "SimSun");
+        exists("新宋体", "SimSun");
         exists("华文行楷", "STXingkai");
         exists("华文仿宋", "STFangsong");
-        exists("仿宋", "FangSong");
+        exists("仿宋", "SimSun");
         exists("幼圆", "YouYuan");
         exists("华文宋体", "STSong");
         exists("华文中宋", "STZhongsong");
@@ -36,7 +43,7 @@ public class Test44_2 {
         exists("方正舒体", "FZShuTi");
         exists("华文细黑", "STXihei");
         exists("宋体扩展", "simsun-extB");
-        exists("仿宋_GB2312", "FangSong_GB2312");
+        exists("仿宋_GB2312", "SimSun");
         exists("新細明體", "SimSun");
         //解决宋体（正文）和宋体（标题）的乱码问题
         // PhysicalFonts.put("PMingLiU",PhysicalFonts.get("SimSun"));
@@ -45,8 +52,8 @@ public class Test44_2 {
 
     public static boolean exists(String label, String key) {
         boolean exists = PhysicalFonts.get(key) != null;
-        if (exists) {
-            System.out.println(label + "存在");
+        if (!exists) {
+            System.out.println(label + "不存在");
         }
 
         return exists;
